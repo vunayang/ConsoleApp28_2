0133f74ba0683046ed22d530acfe088a09fcb12c
vunayang <141714330+vunayang@users.noreply.github.com>
2023-10-07 19:58:46 -0700

    Add files via upload

---
+++ ConsoleApp28/ConsoleApp28/Mengtest.cpp
@@ -0,0 +1,17 @@
+﻿<?xml version="1.0" encoding="utf-8" ?> 
+<xs:schema id="Mengtest" 
+                  targetNamespace="http://tempuri.org/Mengtest.xsd"
+                  elementFormDefault="qualified"
+                  attributeFormDefault="qualified"
+                  xmlns="http://tempuri.org/Mengtest.xsd"
+                  xmlns:mstns="http://tempuri.org/Mengtest.xsd"
+                  xmlns:xs="http://www.w3.org/2001/XMLSchema"
+                  xmlns:msdata="urn:schemas-microsoft-com:xml-msdata"
+                  xmlns:msprop="urn:schemas-microsoft-com:xml-msprop">
+    <xs:element name="Mengtest" msdata:IsDataSet="true" msdata:UseCurrentLocale="true" msprop:EnableTableAdapterManager="true">
+        <xs:complexType>
+            <xs:choice minOccurs="0" maxOccurs="unbounded"></xs:choice>
+        </xs:complexType>
+    </xs:element>
+</xs:schema>
+                                                                 
---
+++ ConsoleApp28/ConsoleApp28/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp28
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
